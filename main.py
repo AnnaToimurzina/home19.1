9303b3926d05c7354ce179a525a632fb8656f7c3
AnnaToimurzina <2402as@gmail,com>
2023-08-01 15:34:18 +0500

    создано веб-приложение

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-<!doctype html>
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+# Для начала определим настройки запуска
+hostName = "localhost" # Адрес для доступа по сети
+serverPort = 8080 # Порт для доступа по сети
+
+class MyServer(BaseHTTPRequestHandler):
+    """
+        Специальный класс, который отвечает за
+        обработку входящих запросов от клиентов
+    """
+    def __get_html_content(self):
+        return """ <!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -289,10 +302,10 @@
                         <div class="card-body">
                             <form>
                                 <div class="mb-3">
-                                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Имя" aria-describedby="emailHelp"> </div>
+                                <input name="name type="text" class="form-control" id="exampleInputEmail1" placeholder="Имя" aria-describedby="emailHelp"> </div>
 
                                 <div class="mb-3">
-                                <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email" aria-describedby="emailHelp">
+                                <input name="email" type="email" class="form-control" id="exampleInputEmail2" placeholder="Email" aria-describedby="emailHelp">
                                     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div><div class="mb-3 form-check">
                                     <input type="checkbox" class="form-check-input" id="exampleCheck1">
                                     <label class="form-check-label" for="exampleCheck1">Check me out</label> </div>
@@ -355,4 +368,28 @@
     })()
 </script>
 </body>
-</html>
+</html> """
+    def do_GET(self):
+        """ Метод для обработки входящих GET-запросов """
+        page_content = self.__get_html_content()
+        self.send_response(200) # Отправка кода ответа
+        self.send_header("Content-type", "index\html") # Отправка типа данных, который будет передаваться
+        self.end_headers() # Завершение формирования заголовков ответа
+        self.wfile.write(bytes(page_content, "utf-8")) # Тело ответа
+
+if __name__ == "__main__":
+    # Инициализация веб-сервера, который будет по заданным параметрам в сети
+    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        # Корректный способ остановить сервер в консоли через сочетание клавиш Ctrl + C
+        pass
+
+    # Корректная остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
+    webServer.server_close()
+    print("Server stopped.")
